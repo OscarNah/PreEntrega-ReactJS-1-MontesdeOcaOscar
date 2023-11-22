7dde7e4c8647525c054bcbe236c2148e9bab008c
OscarNah <1im4montesdeocaoscar@gmail.com>
2023-11-22 03:48:11 -0600

    Se realizo la parte de agregar los componentes, escribir sus codigos y se escribieron en app.jsx

---
+++ Gamer'sHavenHub/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./navbar.css";
+import CartWidget from "../CartWidget/CartWidget";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        <h1 className="nombrePagina"> Gamer's Haven Hub</h1>
+      </div>
+        <div className="categorias">
+          <ul>
+          <li>
+            <a href=""></a> Consolas
+          </li>
+          <li>
+            <a href=""></a> Videojuegos
+          </li>
+          <li>
+            <a href=""></a> Accesorios
+          </li>
+          <li>
+            <div className="carritoBlanco">
+              <CartWidget />
+            </div>
+          </li>
+        </ul>
+        </div>
+        
+    </nav>
+  );
+};
+
+export default NavBar;
